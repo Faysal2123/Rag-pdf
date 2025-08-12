4d2994c99ca370d4ebc14f9ce7dc6b453690bba3
Faysal2123 <foysal2005n1@gmail.com>
2025-08-13 01:04:57 +0600

    second commit

--- server/worker.js
+++ server/worker.js
@@ -9,15 +9,6 @@ const worker = new Worker(
   async (job) => {
     console.log(`Job:`, job.data);
     const data = JSON.parse(job.data);
-    /*
-    Path: data.path
-    read the pdf from path,
-    chunk the pdf,
-    call the openai embedding model for every chunk,
-    store the chunk in qdrant db
-    */
-
-    // Load the PDF
     const loader = new PDFLoader(data.path);
     const docs = await loader.load();
 
